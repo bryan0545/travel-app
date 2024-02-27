3ff835de5f333f33ba5e61b9e91cc435f7d7f78e
Rodas <brayan-rodas@hotmail.com>
2024-02-27 17:26:14 -0500

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Features from '../components/Features';
+import GetApp from '../components/GetApp';
 
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">hello world</h1>
+    <>
+      <Hero />
+    </>
   );
 }
